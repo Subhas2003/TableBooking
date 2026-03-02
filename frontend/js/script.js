24b3bda0287ee84679c220539f1ab8ae317d0175
Subhas2003 <subhasmunain@gmail.com>
2026-03-02 22:53:35 +0530

    Add this

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,5 +1,5 @@
  // Store API Base URL
-        const API_URL = 'http://localhost:5000/api';
+        const API_URL = 'https://tablereserve-api.onrender.com';
 
         let allBookings = [];
         let trendChartInstance = null;
